e999cc7c659ec00eda89c6158a0a3f3ee1f469ef
bibamus <patrick.cassel@googlemail.com>
2014-01-03 23:13:46 +0100

    first needed Obects

--- insula.js
+++ insula.js
@@ -1,6 +1,54 @@
+var game = {
+
+    tick : function(){
+
+    }
+};
 var hero =  {
 
-}
+    thirst : 0,
+    hunger : 0
+};
 var inventar =  {
+    leaf : {
+        name : "leaf",
+        found : 0,
+        amount: 0
+    },
+    stick : {
+        name : "stick",
+        found : 0,
+        amount: 0
+    },
+    cauldron : {
+        name : "cauldron",
+        found : 0,
+        amount: 0
+    },
+    smallStone : {
+        name : "small_stone",
+        found : 0,
+        amount: 0
+    }
 
-}
+
+};
+var map = {
+
+    beach : {
+        obj : beach,
+        found : 1
+    },
+    forest : {
+        obj : forest,
+        found : 1
+    }
+
+};
+var beach = {
+
+};
+var forest = {
+
+
+};
